094ea1e53b69ad0f38758122c0545136177ddc2c
Daniel <didexe@gmail.com>
2017-05-05 23:26:58 +0300

    App and package update

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 var express = require('express'),
-    bodyParser = require('body-parser'),
-    lowdb = require('lowdb');
+  bodyParser = require('body-parser'),
+  low = require('lowdb');
 
-var db = lowdb('./data/data.json');
+var app = express(),
+  db = low('data/data.json');
+
 db._.mixin(require('underscore-db'));
 
-var app = express();
 app.use(bodyParser.json());
-
 app.use(express.static('public'));
 app.use('/libs', express.static('node_modules'));
 
-require('./utils/authorize-user')(app, db);
+var usersRouter = require('./routers/usersRouter')(db);
+var categoriesRouter = require('./routers/categoriesRouter')(db);
 
-//User routes
-var usersController = require('./controllers/users-controller')(db);
-app.get('/api/users', usersController.get);
-app.post('/api/users', usersController.post);
-app.put('/api/auth', usersController.put);
+require('./utils/authorized-user')(app, db);
 
-var port = 3000;
+app.use('/api/users', usersRouter);
+app.use('/api/categories', categoriesRouter);
+
+var port = process.env.PORT || 3013;
+
+
 app.listen(port, function() {
-    console.log('Server is running at http://localhost:' + port);
+  console.log('Server is running at http://localhost:' + port);
 });
