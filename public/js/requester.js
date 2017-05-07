a76526bd878f7af9d390b421bf821f921eef7d01
Daniel <didexe@gmail.com>
2017-05-07 15:01:39 +0300

    Update requester.js

--- public/js/requester.js
+++ public/js/requester.js
@@ -20,9 +20,9 @@ export function post(url, body, headers) {
   return request(url, 'POST', JSON.stringify(body), headers);
 }
 
-// export function put(url, body, headers) {
-//   return request(url, 'PUT', JSON.stringify(body), headers);
-// }
+export function put(url, body, headers) {
+  return request(url, 'PUT', JSON.stringify(body), headers);
+}
 
 export function remove(url, headers) {
   return request(url, 'PUT', '', headers)
